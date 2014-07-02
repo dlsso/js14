e935dc27241d9817b4191a2f0aa8477751f144f4
dlsso <supertonesfan@hotmail.com>
2014-07-01 21:16:32 -0600

    Styled clock

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 $(document).on('ready', function() {
 	var outerShell = $('<div class="outer-shell"></div>');
 	var innerShell = $('<div class="inner-shell"></div>');
-	var leftLabels = $('<div class="left-labels"></div>');
+	var leftLabels = $('<div class="left-labels"><p>PM<p><br><br><p>AUTO</p></div>');
 	var clockScreen = $('<div class="clock-screen"></div>');
 	var amPmDot = $('<div class="am-pm-dot"></div>');
-	var clockText = $('<div class="clock-text"></div>');
-	var amRow = $('<div class="am-row"></div>');
-	var fmRow = $('<div class="fm-row"></div>');
+	var clockText = $('<div class="clock-text">12:17</div>');
+	var amRow = $('<div class="am-row">AM 53 60 70 90 110 140 170 X10 KHz</div>');
+	var fmRow = $('<div class="fm-row">FM 88 92 96 102 106 108 MHz</div>');
 
 	$('.container').append(outerShell)
 	$('.outer-shell').append(innerShell)
-
+	$('.inner-shell').append(clockScreen)
+	$('.clock-screen').append(amPmDot)
+	$('.clock-screen').append(clockText)
+	$('.inner-shell').append(leftLabels)
+	$('.inner-shell').append(amRow)
+	$('.inner-shell').append(fmRow)
 });
 
 
